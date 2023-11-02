798c5939273183048e1e500dae8224639d61d208
Sakshat165 <sakshat.worksite@gmail.com>
2023-11-02 17:08:58 +0530

    EasyBook -3

---
+++ backend/models/Notes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const noteSchema = new Schema({
+   tile:{
+    type:String,
+    required:true
+   },
+   description:{
+    type:String,
+    required:true,
+   },
+   tag:{
+    type:String ,
+    default:"General"
+   },
+   date:{
+    type:Date,
+    default:Date.now
+   }
+   
+    
+  });
+
+
+  module.exports=mongoose.model('notes',noteSchema);
