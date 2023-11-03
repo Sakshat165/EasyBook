d0ca7cfa9b8822686fb6dc62519b7579c9f5b11c
Sakshat165 <sakshat.worksite@gmail.com>
2023-11-03 11:45:14 +0530

    EasyBook - 4

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,54 @@
-const express=require('express');
-const User = require('../models/User');
-const router=express.Router();
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const { body, validationResult } = require("express-validator");
+const JWT_SECRET='Sakshatisagoodb$oy';
 
-router.post('/',(req,res)=>
-{
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
-    
-})
+//New User using :POST "/api/auth/createuser". No login required
 
-module.exports= router 
+//Validation
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter Valid Name").isLength({ min: 3 }),
+    body("email", "Enter Valid Email").isEmail(),
+    body("password", "Enter Valid Password").isLength({ min: 3 }),
+  ],
+  async (req, res) => {
+    //Error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+    //Hashing Password to avoid hacking
+        const salt = bcrypt.genSaltSync(10);
+        const SecPass = bcrypt.hashSync(req.body.password, salt);
+    //Create new User
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: SecPass
+      });
+      //creating tokens
+      const data={
+        user:{
+           id: user.id
+        }
+      }
+      const authtoken=jwt.sign(data,JWT_SECRET);
+      res.json({authtoken});
+    }
+    catch (error) {
+      if (error.code === 11000) {
+        // Duplicate key error
+        return res.status(400).json({ error: "Email already exists" });
+      }
+      res.status(500).json({ error: "Server error" });
+    }
+  }
+);
+
+module.exports = router;
